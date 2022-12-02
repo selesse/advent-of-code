e4e638ce7f0e030ba8cbadbf3601d746eaf872eb
Alex Selesse <alex@selesse.com>
2022-12-02 10:17:15 -0500

    Add missing test for part 1

--- 2022/src/test/java/Day1Test.java
+++ 2022/src/test/java/Day1Test.java
@@ -4,10 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day1Test {
     @Test
-    public void canReturnSolution() {
+    public void canReturnSolutionForPart1() {
         Day1 day1 = new Day1();
         int maxCalories = day1.part1Solution();
 
         assertThat(maxCalories).isEqualTo(24000);
+    }
+
+    @Test
+    public void canReturnSolutionForPart2() {
+        Day1 day1 = new Day1();
+        int maxCalories = day1.part2Solution();
+
+        assertThat(maxCalories).isEqualTo(45000);
     }
 }
