9b08ffe5d018121f689bef8d8f45d7a5bb33f1ab
Alex Selesse <alex@selesse.com>
2022-12-08 15:13:01 -0500

    Day 8 solution, part 2

--- 2022/src/main/java/Day8.java
+++ 2022/src/main/java/Day8.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -7,6 +8,9 @@ public class Day8 extends AocChallenge {
     public void run() {
         System.out.println("Part 1");
         System.out.println(part1());
+
+        System.out.println("Part 2");
+        System.out.println(part2());
     }
 
     public int part1() {
@@ -18,6 +22,13 @@ public class Day8 extends AocChallenge {
         int interiorEdges = forest.interiorVisibleEdges();
 
         return visibleEdges + interiorEdges;
+    }
+
+    public int part2() {
+        var lines = getLinesForPart1();
+
+        Forest forest = getForestFromInput(lines);
+        return forest.maxScenicScore();
     }
 
     private Forest getForestFromInput(List<String> lines) {
@@ -52,39 +63,99 @@ public class Day8 extends AocChallenge {
             return (int) visibilities.stream().filter(x -> x.totalVisible() >= 1).count();
         }
 
-        private Visible computeVisibility(int initialX, int initialY) {
-            int currentValue = trees[initialX][initialY];
+        private Visible computeVisibility(int x, int y) {
+            int currentValue = trees[x][y];
 
+            boolean north = computeNorthTrees(x, y).stream().allMatch(tree -> tree < currentValue);
+            boolean south = computeSouthTrees(x, y).stream().allMatch(tree -> tree < currentValue);
+            boolean east = computeEastTrees(x, y).stream().allMatch(tree -> tree < currentValue);
+            boolean west = computeWestTrees(x, y).stream().allMatch(tree -> tree < currentValue);
+
+            return new Visible(x, y, currentValue, north, south, east, west);
+        }
+
+        public int maxScenicScore() {
+            List<ScenicScore> scenicScores = new ArrayList<>();
+            for (int x = 1; x < width - 1; x++) {
+                for (int y = 1; y < height - 1; y++) {
+                    scenicScores.add(computeScenicScore(x, y));
+                }
+            }
+            return scenicScores.stream().map(ScenicScore::totalScore).max(Integer::compareTo).orElseThrow();
+        }
+
+        private ScenicScore computeScenicScore(int x, int y) {
+            int currentValue = trees[x][y];
+
+            var northTrees = computeNorthTrees(x, y);
+            var southTrees = computeSouthTrees(x, y);
+            var eastTrees = computeEastTrees(x, y);
+            var westTrees = computeWestTrees(x, y);
+            // Thanks Java!!!!
+            Collections.reverse(northTrees);
+            Collections.reverse(southTrees);
+            Collections.reverse(eastTrees);
+            Collections.reverse(westTrees);
+            int north = computeVisibilityCount(currentValue, northTrees);
+            int south = computeVisibilityCount(currentValue, southTrees);
+            int east = computeVisibilityCount(currentValue, eastTrees);
+            int west = computeVisibilityCount(currentValue, westTrees);
+
+            return new ScenicScore(x, y, currentValue, north, south, east, west);
+        }
+
+        private int computeVisibilityCount(int value, List<Integer> trees) {
+            int count = 0;
+            for (Integer tree : trees) {
+                count++;
+                if (tree >= value) {
+                    break;
+                }
+            }
+            return count;
+        }
+
+        private List<Integer> computeNorthTrees(int x, int y) {
             List<Integer> northTrees = new ArrayList<>();
+            for (int i = 0; i < x; i++) {
+                northTrees.add(trees[i][y]);
+            }
+            return northTrees;
+        }
+
+        private List<Integer> computeSouthTrees(int x, int y) {
             List<Integer> southTrees = new ArrayList<>();
+            for (int i = width - 1; i > x; i--) {
+                southTrees.add(trees[i][y]);
+            }
+            return southTrees;
+        }
+
+        private List<Integer> computeEastTrees(int x, int y) {
             List<Integer> eastTrees = new ArrayList<>();
+            for (int j = height - 1; j > y; j--) {
+                eastTrees.add(trees[x][j]);
+            }
+            return eastTrees;
+        }
+        private List<Integer> computeWestTrees(int x, int y) {
             List<Integer> westTrees = new ArrayList<>();
-
-            for (int j = height - 1; j > initialY; j--) {
-                eastTrees.add(trees[initialX][j]);
+            for (int j = 0; j < y; j++) {
+                westTrees.add(trees[x][j]);
             }
-            for (int j = 0; j < initialY; j++) {
-                westTrees.add(trees[initialX][j]);
-            }
-            for (int i = width - 1; i > initialX; i--) {
-                southTrees.add(trees[i][initialY]);
-            }
-            for (int i = 0; i < initialX; i++) {
-                northTrees.add(trees[i][initialY]);
-            }
-
-            boolean north = northTrees.stream().allMatch(tree -> tree < currentValue);
-            boolean south = southTrees.stream().allMatch(tree -> tree < currentValue);
-            boolean east = eastTrees.stream().allMatch(tree -> tree < currentValue);
-            boolean west = westTrees.stream().allMatch(tree -> tree < currentValue);
-
-            return new Visible(initialX, initialY, currentValue, north, south, east, west);
+            return westTrees;
         }
     }
 
     public record Visible(int x, int y, int currentValue, boolean north, boolean south, boolean east, boolean west) {
         public int totalVisible() {
             return Stream.of(north, south, east, west).map(x -> x ? 1 : 0).reduce(0, Integer::sum);
+        }
+    }
+
+    public record ScenicScore(int x, int y, int currentValue, int north, int south, int east, int west) {
+        public int totalScore() {
+            return north * south * east * west;
         }
     }
 }
--- 2022/src/test/java/Day8Test.java
+++ 2022/src/test/java/Day8Test.java
@@ -8,4 +8,10 @@ public class Day8Test {
         Day8 day8 = new Day8();
         assertThat(day8.part1()).isEqualTo(21);
     }
+
+    @Test
+    public void canReturnSolutionForPart2() {
+        Day8 day8 = new Day8();
+        assertThat(day8.part2()).isEqualTo(8);
+    }
 }
